0b46c02c9786d34a7d9f63472be2c30e79d36015
Alfonso Boizas Crespo <boizasal@gmail.com>
2025-12-07 14:29:29 +0100

    Initial commit

---
+++ Demo-visionOS/Demo_visionOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_visionOSApp.swift
+//  Demo-visionOS
+//
+//  Created by Alfonso Boizas Crespo on 7/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Demo_visionOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Demo-visionOSTests/Demo_visionOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_visionOSTests.swift
+//  Demo-visionOSTests
+//
+//  Created by Alfonso Boizas Crespo on 7/12/25.
+//
+
+import Testing
+@testable import Demo_visionOS
+
+struct Demo_visionOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
